80f3678979eca86bc95b9cb63eb282e266ddbac3
Rounak Agrawal <rounak1430@gmail.com>
2023-05-20 19:06:13 +0530

    home removed

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const ScreenInitializer(),
       routes: {
         '/': (context) => const ScreenInitializer(),
         ScreenOne.routename: (context) => const ScreenOne(),
